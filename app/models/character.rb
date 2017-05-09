79e57e86f0d572af62a284386564b27a10def2f0
Amy Collins <amy.collins@user.noreply.github.com>
2017-05-08 19:46:56 -0500

    commit 2

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,10 @@
 class Character < ApplicationRecord
 belongs_to(:actor, :class_name =>"Actor", :foreign_key=>"actor_id")
+belongs_to(:movie, :class_name=>"Movie", :foreign_key=>"movie_id")
+
   #- name: must be present; must be unique in combination with dob
-  validates :name, :presence => true, :uniqueness => {:scope=>:dob}
+  validates :actor_id, :presence => true
+  validates :movie_id, :presence => true
 
    #- dob: no rules
    #- bio: no rules
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
 belongs_to(:director, :class_name=>"Director", :foreign_key=>"director_id")
 has_many(:characters, :class_name=>"Character", :foreign_key=>"movie_id")
-has_many(:actors, :class_name=>"Actor", :foreign_key=>"movie_id")
+
 
 #  - director_id: must be present
 validates :director_id, :presence => true
